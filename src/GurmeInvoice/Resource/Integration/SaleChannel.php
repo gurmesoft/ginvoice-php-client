551432cfe13fe71572e0783d6937c91e593c58b5
Fikret Çin <fikret@gurmesoft.com>
2025-04-18 07:50:04 +0000

    salechannel

---
+++ src/GurmeInvoice/Resource/Integration/SaleChannel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace GurmeInvoice\Resource\Integration;
+
+use GurmeInvoice\Resource\Base;
+
+class SaleChannel extends Base
+{
+    public string $endpoint = 'integrations/sale-channel';
+}
